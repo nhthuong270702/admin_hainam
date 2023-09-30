229f318c6d71119250cdd8be17828d3079477702
TranVietDu <tranvietdu054@gmail.com>
2023-09-30 12:30:05 +0700

    update

--- resources/views/pages/product/list.blade.php
+++ resources/views/pages/product/list.blade.php
@@ -76,7 +76,7 @@
                             </tbody>
                         </table>
                         <div class="mt-4 mb-4" style=" display: flex; justify-content: center;">
-                            {!! $products->links() !!}
+                            {{ $products->appends($_GET)->links() }}
                         </div>
                     </div>
                 </div>
